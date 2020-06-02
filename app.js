d115a0af2a187c6ad337e7b82d95f754f609d1c9
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2020-06-03 00:29:55 +0530

    Search feature added

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ const axios = require("axios").default;
 
 app.set("view engine", "ejs");
 
-const API = "http://omdbapi.com/?apikey=thewdb&s=trump";
-
 app.listen(3002, function () {
   console.log("Movie App Server is now listening.....");
 });
 
 //creating routes
 
+//search route
+app.get("/", function (req, res) {
+  res.render("search.ejs");
+});
+
+//result route
 app.get("/results", function (req, res) {
   //we now use axios library to make an API call to the given address
 
+  //save the term entered by the user
+  var term = req.query.movie;
+
+  const API = "http://omdbapi.com/?apikey=thewdb&s=" + term;
   axios
     .get(API)
     .then(function (response) {
